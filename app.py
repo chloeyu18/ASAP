da64aed4fe00b3eab5972ea165c870a75cf20f46
David Yu <ChloeYu@Chloes-MacBook-Pro-6.local>
2019-01-24 20:16:04 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/artsfirst')
+def artsfirst():
+    return render_template("artsfirst.html")
+
+@app.route('/board')
+def board():
+    return render_template("board.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/eastside')
+def eastside():
+    return render_template("eastside.html")
+
+@app.route('/openmic')
+def openmic():
+    return render_template("openmic.html")
+
+@app.route('/productions')
+def productions():
+    return render_template("productions.html")
+
+@app.route('/spotlight')
+def spotlight():
+    return render_template("spotlight.html")
